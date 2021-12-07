6a5e4a5b9db6bbe69962541143efd9eb8d4bed35
Victor-Zaragoza <82984092+Victor-Zaragoza@users.noreply.github.com>
2021-12-06 18:01:17 -0600

    nuevo

--- CompruebaSesion.php
+++ CompruebaSesion.php
@@ -1,35 +1,33 @@
 <?php
-    session_start();
-    include('mysql/MysqlConn.php');
-    $conn= conectar();
-    
-    
-    if(isset($_POST['AgregaCarrito'])){
-        if (isset($_SESSION['usuario'])){
-            $id_sesion= array_column($_SESSION['usuario'], "id_platillo");
-            
-            if(!in_array($_GET['id_platillo'], $id_sesion)){
-                $datos_de_sesion= array(
-                'id_platillo' => $_GET['id_platillo'],
-                'nombre' => $_POST['nombre'],
-                'precio' => $_POST['precio'],
-                'cantidad' => $_POST['cantidad']
-            );
-            
-            $_SESSION['usuario'][]= $datos_de_sesion;
-            }
-        }
-        else{
-            $datos_de_sesion= array(
-                'id_platillo' => $_GET['id_platillo'],
-                'nombre' => $_POST['nombre'],
-                'precio' => $_POST['precio'],
-                'cantidad' => $_POST['cantidad']
-            );
-            
-            $_SESSION['usuario'][]= $datos_de_sesion;
-            
-        }
-   }
+//    include('mysql/MysqlConn.php');
+//    $conn= conectar();
+//    
+//    if(isset($_POST['AgregaCarrito'])){
+//        if (isset($_SESSION['carrito'])){
+//            $id_sesion= array_column($_SESSION['carrito'], "id_platillo");
+//            
+//            if(!in_array($_GET['id_platillo'], $id_sesion)){
+//                $datos_de_sesion= array(
+//                'id_platillo' => $_GET['id_platillo'],
+//                'nombre' => $_POST['nombre'],
+//                'precio' => $_POST['precio'],
+//                'cantidad' => $_POST['cantidad']
+//            );
+//            
+//            $_SESSION['carrito'][]= $datos_de_sesion;
+//            }
+//        }
+//        else{
+//            $datos_de_sesion= array(
+//                'id_platillo' => $_GET['id_platillo'],
+//                'nombre' => $_POST['nombre'],
+//                'precio' => $_POST['precio'],
+//                'cantidad' => $_POST['cantidad']
+//            );
+//            
+//            $_SESSION['carrito'][]= $datos_de_sesion;
+//            
+//        }
+//   }
     
  ?>
